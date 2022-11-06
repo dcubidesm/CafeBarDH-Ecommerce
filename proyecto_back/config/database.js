6120a4bd560706f011402aeee32b9577921139d9
Cafe Bar DH <111820300+dcubidesm@users.noreply.github.com>
2022-11-06 17:07:50 -0500

    Actualizacion back_end
    Se actualiza los archivos de back_end

--- proyecto_back/config/database.js
+++ proyecto_back/config/database.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");//dependencia mongobd para conexion a la base de datos y creacion
 const host = "localhost";
 const port = "27017";
-const db = "dh";
+const db = "cafebardh";
+// proceso de conexion con mongodb y que utilice las variables creadas anteriormente
 
 exports.mongoConnect = () => {
     const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
-    
     mongoose.connect(mongoStringConnection);
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
